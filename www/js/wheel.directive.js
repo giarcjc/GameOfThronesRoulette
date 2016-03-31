fd4f5900e52c01c7a4d20282e3d7d80609981338
chriscraig <chris@cjcraig.com>
2016-03-31 00:16:41 -0600

    cleanup

--- www/js/wheel.directive.js
+++ www/js/wheel.directive.js
@@ -7,32 +7,8 @@
       templateUrl: 'js/wheel.tmpl.html',
       link: function(scope, elem, attrs){
         scope.canvas = elem.find('myCanvas')[0];
-        console.log(this);
+        var spinWheel
         var counter = false;
-
-        var spinWheel = new Winwheel({
-         'canvasId' : 'myCanvas',
-         'numSegments': 10,
-         'outerRadius' : 170,
-         'lineWidth': 3,
-         'segments': [
-           {'fillStyle' : '#eae56f', 'text' : 'Episode 1'},
-           {'fillStyle' : '#89f26e', 'text' : 'Episode 2'},
-           {'fillStyle' : '#7de6ef', 'text' : 'Episode 3'},
-           {'fillStyle' : '#e7706f', 'text' : 'Episode 4'},
-           {'fillStyle' : '#eae56f', 'text' : 'Episode 5'},
-           {'fillStyle' : '#89f26e', 'text' : 'Episode 6'},
-           {'fillStyle' : '#7de6ef', 'text' : 'Episode 7'},
-           {'fillStyle' : '#e7706f', 'text' : 'Episode 8'},
-           {'fillStyle' : '#eae56f', 'text' : 'Episode 9'},
-           {'fillStyle' : '#e7706f', 'text' : 'Episode 10'}
-         ],
-         'animation' : {
-           'type' : 'spinToStop',
-           'duration' : 5,
-           'spins' : 8
-         }
-       });
 
        scope.spin = function(){
          if(!counter){
@@ -43,6 +19,31 @@
            return spinWheel.startAnimation();
          }
        };
+
+       spinWheel = new Winwheel({
+        'canvasId' : 'myCanvas',
+        'numSegments': 10,
+        'outerRadius' : 170,
+        'lineWidth': 3,
+        'segments': [
+          {'fillStyle' : '#eae56f', 'text' : 'Episode 1'},
+          {'fillStyle' : '#89f26e', 'text' : 'Episode 2'},
+          {'fillStyle' : '#7de6ef', 'text' : 'Episode 3'},
+          {'fillStyle' : '#e7706f', 'text' : 'Episode 4'},
+          {'fillStyle' : '#eae56f', 'text' : 'Episode 5'},
+          {'fillStyle' : '#89f26e', 'text' : 'Episode 6'},
+          {'fillStyle' : '#7de6ef', 'text' : 'Episode 7'},
+          {'fillStyle' : '#e7706f', 'text' : 'Episode 8'},
+          {'fillStyle' : '#eae56f', 'text' : 'Episode 9'},
+          {'fillStyle' : '#e7706f', 'text' : 'Episode 10'}
+        ],
+        'animation' : {
+          'type' : 'spinToStop',
+          'duration' : 5,
+          'spins' : 8
+         }
+       });
+
 
       }
     }
